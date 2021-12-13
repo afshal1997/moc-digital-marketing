d94a196d2023ea2b64d9b1396efa2aa31584db9e
Afshal Warsi <afshal.warsi@shottech.co>
2021-12-14 02:39:17 +0500

    home Page Completed

---
+++ includes/jsLinks.php
@@ -0,0 +1,27 @@
+<script src="https://use.fontawesome.com/a5749b4813.js"></script>
+
+<script src="javascript/jquery.min.js"></script>
+<script src="javascript/countto.js"></script>
+<script src="javascript/plugins.js"></script>
+<script src="javascript/shortcodes.js"></script>
+<script src="javascript/jquery.fancybox.js"></script>
+<script src="javascript/main.js"></script>
+<script src="javascript/owl.carousel.min.js"></script>
+<script src="javascript/animsition.js"></script>
+<script src="javascript/wow.min.js"></script>
+<script src="includes/rev-slider/js/jquery.themepunch.tools.min.js"></script>
+<script src="includes/rev-slider/js/jquery.themepunch.revolution.min.js"></script>
+<script src="javascript/rev-slider.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.actions.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.migration.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="includes/rev-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+</body>
+
+</html>
